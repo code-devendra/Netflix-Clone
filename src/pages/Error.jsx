96b6ffef439a3acd949ccb1e7f293bf04fb7d682
code-devendra <devendrakhinchi.work@gmail.com>
2023-07-28 17:17:28 +0530

    Add error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="hero min-h-screen">
+      <div className="hero-content text-center text-white">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Oops! 404</h1>
+          <p className="py-6">The page you are looking is not exist..</p>
+          <Link to="/" className="btn text-white bg-red-500 hover:bg-red-600">
+            Go to Home Page
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
