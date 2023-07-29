784dad080188015c1d8d099eed26566f20a7cda3
code-devendra <devendrakhinchi.work@gmail.com>
2023-07-29 17:31:17 +0530

    perform some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,25 +3,55 @@ const Footer = () => {
     <>
       <footer className="footer p-10 text-slate-200 underline">
         <div>
-          <a className="link link-hover">FAQ</a>
-          <a className="link link-hover">Media Center</a>
-          <a className="link link-hover">Ways to Watch</a>
-          <a className="link link-hover">Cookie Preferences</a>
-          <a className="link link-hover">Speed Test</a>
+          <a href="#" className="link link-hover">
+            FAQ
+          </a>
+          <a href="#" className="link link-hover">
+            Media Center
+          </a>
+          <a href="#" className="link link-hover">
+            Ways to Watch
+          </a>
+          <a href="#" className="link link-hover">
+            Cookie Preferences
+          </a>
+          <a href="#" className="link link-hover">
+            Speed Test
+          </a>
         </div>
         <div>
-          <a className="link link-hover">Help Center</a>
-          <a className="link link-hover">Investor Relations</a>
-          <a className="link link-hover">Terms of Use</a>
-          <a className="link link-hover">Corporate Information</a>
-          <a className="link link-hover">Legal Notices</a>
+          <a href="#" className="link link-hover">
+            Help Center
+          </a>
+          <a href="#" className="link link-hover">
+            Investor Relations
+          </a>
+          <a href="#" className="link link-hover">
+            Terms of Use
+          </a>
+          <a href="#" className="link link-hover">
+            Corporate Information
+          </a>
+          <a href="#" className="link link-hover">
+            Legal Notices
+          </a>
         </div>
         <div>
-          <a className="link link-hover">Account</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Privacy</a>
-          <a className="link link-hover">Contact Us</a>
-          <a className="link link-hover">Only on Netflix</a>
+          <a href="#" className="link link-hover">
+            Account
+          </a>
+          <a href="#" className="link link-hover">
+            Jobs
+          </a>
+          <a href="#" className="link link-hover">
+            Privacy
+          </a>
+          <a href="#" className="link link-hover">
+            Contact Us
+          </a>
+          <a href="#" className="link link-hover">
+            Only on Netflix
+          </a>
         </div>
       </footer>
       <footer className="footer px-10 pb-12 text-slate-300 ">
